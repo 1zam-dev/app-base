abb2023150c497093a6aca3f65183d0255f93eae
Blake <hnamnguyen.hy@gmail.com>
2022-10-07 17:45:12 +0700

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import "./App.css";
+import TodoListView from "./module/Todo/Presentation/Todo/TodoList/TodoListView";
+
+function App() {
+  return (
+    <div className="App">
+      <TodoListView />
+    </div>
+  );
+}
+
+export default App;
