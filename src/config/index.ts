ead6203a1fff1f41ec92452780cabcff1796468c
blakecoding <hnamnguyen.hy@gmail.com>
2022-10-11 02:21:23 +0700

    feat axios

---
+++ src/config/index.ts
@@ -0,0 +1,3 @@
+export const APP_STORAGE = {
+  ACCESS_TOKEN: 'DBS_ACCESS_TOKEN',
+};
---
+++ src/services/defaultAxios.ts
@@ -0,0 +1,47 @@
+import axios from 'axios';
+import { APP_STORAGE } from 'src/config/index';
+
+export default class DefaultAxios {
+    api;
+
+    constructor(baseURL: string, contentType?: string) {
+        this.api = axios.create({
+            baseURL,
+            headers: {
+                'Content-Type': contentType ? contentType : 'application/json',
+            },
+        });
+    }
+
+    setToken = () => {
+        const token = localStorage.getItem(APP_STORAGE.ACCESS_TOKEN);
+        if (token) {
+            this.api.defaults.headers.common.Authorization = `Bearer ${token}`;
+        }
+    };
+
+    get = <T = any>(url = '', config = {}) => {
+        this.setToken();
+        return this.api.get<T>(url, config);
+    };
+
+    post = <T = any>(url = '', body = {}, config = {}) => {
+        this.setToken();
+        return this.api.post<T>(url, body, config);
+    };
+
+    patch = <T = any>(url = '', body = {}, config = {}) => {
+        this.setToken();
+        return this.api.patch<T>(url, body, config);
+    };
+
+    put = (url = '', body = {}, config = {}) => {
+        this.setToken();
+        return this.api.put(url, body, config);
+    };
+
+    delete = (url = '', config = {}) => {
+        this.setToken();
+        return this.api.delete(url, config);
+    };
+}
